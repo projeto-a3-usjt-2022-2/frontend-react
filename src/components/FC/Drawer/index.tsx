2a7105b4ce409f9faf675474d4e48f7e72158b2d
guicoelhodev <gs.coelho_dev@outlook.com>
2022-11-13 10:05:23 -0300

    feat: create function component to use as dropdown and navigate between pages

---
+++ src/components/FC/Drawer/index.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  AiFillBook,
+  AiFillCalendar,
+  AiOutlineClose,
+  AiOutlineUser,
+} from "react-icons/ai";
+import { BsFillGrid1X2Fill } from "react-icons/bs";
+
+import styles from "./style.module.scss";
+
+export const Drawer: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  const avaliableLinks = [
+    {
+      path: "/consults",
+      name: "Minhas consultas",
+      icon: <AiFillCalendar />,
+    },
+    {
+      path: "/schedule",
+      name: "Novo agendamento",
+      icon: <AiFillBook />,
+    },
+    {
+      path: "/user",
+      name: "Dados do usuário",
+      icon: <AiOutlineUser />,
+    },
+  ];
+  return (
+    <div className={`${styles.container} ${open ? styles.active : ""}`}>
+      {open ? (
+        <section className={styles.open}>
+          <header>
+            <h2>Acesso Rápido</h2>
+
+            <button
+              className={styles.iconButton}
+              onClick={() => setOpen((prev) => !prev)}
+            >
+              <AiOutlineClose />
+            </button>
+          </header>
+
+          <ul>
+            {avaliableLinks.map((link) => (
+              <li key={link.name}>
+                <Link to={link.path} className={styles.link}>
+                  {link.icon}
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </section>
+      ) : (
+        <section className={styles.closed}>
+          <button
+            className={styles.iconButton}
+            onClick={() => setOpen((prev) => !prev)}
+          >
+            <BsFillGrid1X2Fill />
+          </button>
+        </section>
+      )}
+    </div>
+  );
+};
