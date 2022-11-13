e58352bb85264a749ccd599fb02bf0522ce1cdcd
guicoelhodev <gs.coelho_dev@outlook.com>
2022-11-13 10:07:09 -0300

    feat: create global page when the url path doesn't exists

---
+++ src/pages/Error/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./style.module.scss";
+import ErrorImage from "../../assets/page_not_found.png";
+import { Link } from "react-router-dom";
+
+export const PageError: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <main>
+        <h1>Página não encontrada</h1>
+        <img
+          src={ErrorImage}
+          alt="image representando que a página não foi encontrada"
+        />
+
+        <Link className={styles.link} to="/">
+          Voltar a página inicial
+        </Link>
+      </main>
+    </div>
+  );
+};
