ab9c7cb8dbfa1395dfbb578c7b5a561160191799
guicoelhodev <gs.coelho_dev@outlook.com>
2022-11-19 22:47:20 -0300

    feat: create button to disconnect the currentUser

--- src/components/FC/Layout/index.tsx
+++ src/components/FC/Layout/index.tsx
@@ -1,8 +1,8 @@
 import React, { ReactNode } from "react";
 import { Drawer } from "../Drawer";
 import styles from "./style.module.scss";
-import Logout from "../../../assets/img/logout.png"
-import Vector from "../../../assets/img/Vector.png"
+import Logout from "../../../assets/img/logout.png";
+import Vector from "../../../assets/img/Vector.png";
 
 interface ILayout {
   children: ReactNode;
@@ -14,8 +14,14 @@ export const Layout: React.FC<ILayout> = ({ children, title }) => {
       <header className={styles.header}>
         <h1>{title}</h1>
         <div className={styles.homeButtons}>
-          <a href="/login"><img src={Vector} alt="config" /></a>
-          <a href="/login"><img src={Logout} alt="logout" /></a>
+          <a href="/login">
+            <img src={Vector} alt="config" />
+          </a>
+          <button
+            onClick={() => sessionStorage.removeItem("@USER_CREDENTIALS")}
+          >
+            <img src={Logout} alt="logout" />
+          </button>
         </div>
       </header>
       <aside className={styles.drawer}>
