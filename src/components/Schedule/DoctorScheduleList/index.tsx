8747e09430f6fdf348dd77c806684197620de7b0
guicoelhodev <gs.coelho_dev@outlook.com>
2022-11-19 22:47:54 -0300

    feat: create DoctorSchedule component

--- src/components/Schedule/DoctorScheduleList/index.tsx
+++ src/components/Schedule/DoctorScheduleList/index.tsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import styles from "./style.module.scss";
 
 interface IDoctorSchedule {
+  currentHour: string;
+  setCurrentHour: Dispatch<
+    SetStateAction<{
+      date: string;
+      hour: string;
+    }>
+  >;
   doctorSchedule: string[];
 }
 export const DoctorScheduleList: React.FC<IDoctorSchedule> = ({
   doctorSchedule,
+  currentHour,
+  setCurrentHour,
 }) => {
   return (
     <div className={styles.container}>
       {doctorSchedule?.map((item) => (
-        <button>{item}</button>
+        <button
+          className={`${currentHour === item ? styles.active : ""}`}
+          onClick={(e) => {
+            e.preventDefault();
+            setCurrentHour((prev) => ({ ...prev, hour: item }));
+          }}
+        >
+          {item}
+        </button>
       ))}
     </div>
   );
