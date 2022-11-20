30ca371189d19c02350ed3fb097738180802eecd
guicoelhodev <gs.coelho_dev@outlook.com>
2022-11-19 22:48:32 -0300

    feat: add new function into Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { BsFillCheckCircleFill, BsFillStopwatchFill } from "react-icons/bs";
 import { Layout } from "../../components/FC/Layout";
+import { ConsultCard } from "../../components/Home/ConsultCard";
+import { useAuthentication } from "../../hooks/useAuthentication";
+import { allConsultsById } from "../../services/POST/allConsultsById";
+
 import styles from "./style.module.scss";
 
 const statusConsult = {
@@ -23,47 +27,29 @@ type ICard = {
   modality: string;
   date: string;
   clinic: string;
-  street: string;
-  hours: string;
+  cep: string;
+  hour: string;
   status: keyof typeof statusConsult;
 };
 export const Home: React.FC = () => {
-  const name = "Guilherme Coelho";
+  const [consults, setConsults] = useState<[] | ICard[]>([]);
 
-  const cards: ICard[] = [
-    {
-      modality: "Ortopedia",
-      date: "21/03/2022",
-      clinic: "Amil Tatuapé",
-      street: "R. Visc de Itaboraí, 60 - Vila Azevedo",
-      hours: "14:30",
-      status: "waiting",
-    },
-    {
-      modality: "Ortopedia",
-      date: "21/03/2022",
-      clinic: "Amil Tatuapé",
-      street: "R. Visc de Itaboraí, 60 - Vila Azevedo",
-      hours: "14:30",
-      status: "done",
-    },
-    {
-      modality: "Ortopedia",
-      date: "21/03/2022",
-      clinic: "Amil Tatuapé",
-      street: "R. Visc de Itaboraí, 60 - Vila Azevedo",
-      hours: "14:30",
-      status: "cancelled",
-    },
-  ];
+  const { userLogged } = useAuthentication();
 
-  const formatDate = (date: string, hours: string) => {
-    return `${date.slice(0, 5)} - ${hours}`;
+  const getConsults = async () => {
+    let { data } = await allConsultsById({
+      clinic: userLogged.clinic,
+      crm: userLogged.crm,
+      userId: userLogged.id,
+    });
+
+    setConsults(data);
   };
 
-  const getSvgClass = (type: string) => {
-    return `${styles[type]}`;
-  };
+  useEffect(() => {
+    getConsults();
+  }, []);
+
   return (
     <Layout title={`Bem vindo, ${name}`}>
       <div className={styles.container}>
@@ -71,22 +57,15 @@ export const Home: React.FC = () => {
         <main className={styles.main}>
           <h3>Minhas consultas</h3>
 
-          <ul>
-            {cards.map((card) => (
-              <li className={styles.cardConsult}>
-                <h3>{card.modality}</h3>
-
-                <span>{formatDate(card.date, card.hours)}</span>
-
-                <h4>{card.clinic}</h4>
-                <p>{card.street}</p>
-
-                <aside className={getSvgClass(statusConsult[card.status].type)}>
-                  {statusConsult[card.status].svg}
-                </aside>
-              </li>
-            ))}
-          </ul>
+          {consults.length > 0 ? (
+            <ul>
+              {consults.map((card) => (
+                <ConsultCard {...card} />
+              ))}
+            </ul>
+          ) : (
+            <div>Sem consultas até o momento :(</div>
+          )}
         </main>
       </div>
     </Layout>
